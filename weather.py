2973021915f8f750fb26485f5fb9025bd27af8c0
Stefan <st.krstic@gmail.com>
2020-01-22 11:54:29 +0100

    add config.ini

--- weather.py
+++ weather.py
@@ -5,20 +5,20 @@ import pyowm
 import weather_display_model
 import configparser
 
-# read api key from api.ini
+# read api key from config.ini
 config = configparser.ConfigParser()
-config.read('api.ini')
+config.read('config.ini')
 api_key = config.get("api_key")
 
 # enter information
-location = "London"
-measurementSystem = 0 # 0 - Metric, 1 - Imperial
-update_interval = 15 # update interval in minutes
+location = config.get("location")
+measurement_unit = config.get("measurement_unit")  # 0 - Metric, 1 - Imperial
+update_interval = config.get("update_interval")  # update interval in minutes
 
 # import necessary API keys from OWM
-owm = pyowm.OWM("Your API") # API Key here
+owm = pyowm.OWM(api_key)  # API Key here
 observation = owm.weather_at_place(location)
-wd = weather_display_model.WeatherDisplay(location, measurementSystem)
+wd = weather_display_model.WeatherDisplay(location, measurement_unit)
 
 try:
     while True:
@@ -28,7 +28,7 @@ try:
         loop_end_time = time.time() + 60 * update_interval
         while time.time() < loop_end_time:
             wd.print_info_to_display()
-            
-except KeyboardInterrupt: 
+
+except KeyboardInterrupt:
     print("Ending!")
     # lcd.lcd_clear()
